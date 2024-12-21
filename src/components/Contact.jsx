314179edd008f87250822f3df92bae6ac6827bf8
Ilknur <ilknurtuncer67@gmail.com>
2024-12-22 02:41:03 +0300

    contact map

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,85 +1,19 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useTranslation } from 'react-i18next'; // useTranslation ekledik
-
-const contactMethods = [
-  {
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-        />
-      </svg>
-    ),
-    contact: "mail-adresi.....",
-  },
-  {
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
-        />
-      </svg>
-    ),
-    contact: "tel.......",
-  },
-  {
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-        />
-      </svg>
-    ),
-    contact: "adres....",
-  },
-];
+import { useTranslation } from "react-i18next";
 
 const Contact = () => {
-  const { t } = useTranslation(); // useTranslation hook'u ile t fonksiyonu ekleniyor
+  const { t } = useTranslation();
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     company: "",
     message: "",
   });
-  const navigate = useNavigate(); // useNavigate
+  const navigate = useNavigate();
 
   const submitForm = async (e) => {
     e.preventDefault();
-
     try {
       const response = await fetch(
         "https://nodeijs-mailler01-3hsx.vercel.app/sendmail",
@@ -91,11 +25,10 @@ const Contact = () => {
           body: JSON.stringify(formData),
         }
       );
-
       if (response.ok) {
         const result = await response.json();
         console.log(result);
-        navigate("/contact-success"); // Form gönderimi başarılıysa yönlendirme yap
+        navigate("/contact-success");
       } else {
         console.error("Form data submission failed");
       }
@@ -119,79 +52,95 @@ const Contact = () => {
 
   return (
     <main className="py-14 bg-gray-100">
-      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
-        <div className="max-w-lg mx-auto gap-12 justify-between lg:flex lg:max-w-none">
-          <div className="max-w-lg space-y-3 text-xl font-semibold text-gray-600 mt-12">
+      <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8 flex flex-col lg:flex-row">
+        {/* Sol taraf: Harita ve Başlık */}
+        <div className="lg:w-1/3 flex flex-col space-y-4">
+          <h3 className="text-teal-500 text-3xl font-semibold sm:text-4xl">
+            {t("contact.address")}
+          </h3>
+          <div className="aspect-video rounded-lg overflow-hidden shadow-lg">
+            <iframe
+              title="Google Maps"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.835434509616!2d-122.42067948467774!3d37.77492977975817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085809c6b5ed4a5%3A0x4e84d8f1e6e8dc2b!2sSan%20Francisco%2C%20CA%2C%20USA!5e0!3m2!1sen!2str!4v1602105413461!5m2!1sen!2str"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+        </div>
+
+        {/* Sağ taraf: Form ve iletişim bilgileri */}
+        <div className="lg:w-2/3 lg:pl-12 mt-12 lg:mt-0">
+          <div className="space-y-3 text-xl font-semibold text-gray-600">
             <h3 className="text-teal-500 text-3xl font-semibold sm:text-4xl">
-              {t("contact.title")} {/* Bu satırda dil çevirisi ekledik */}
+              {t("contact.title")}
             </h3>
-            <p className="text-gray-500 mt-4">
-              {t("contact.subtitle")} {/* Bu satırda dil çevirisi ekledik */}
-            </p>
+            <p className="text-gray-500 mt-4">{t("contact.subtitle")}</p>
+          </div>
+          <form
+            onSubmit={submitForm}
+            className="mt-6 space-y-5 bg-white p-6 rounded-lg shadow-lg"
+          >
             <div>
-              <ul className="mt-6 flex flex-wrap gap-x-10 gap-y-6 items-center">
-                {contactMethods.map((item, idx) => (
-                  <li key={idx} className="flex items-center gap-x-3">
-                    <div className="flex-none text-gray-400">{item.icon}</div>
-                    <p className="text-gray-600">{item.contact}</p>
-                  </li>
-                ))}
-              </ul>
+              <label className="font-medium text-lg text-gray-700">
+                {t("contact.form.name")}
+              </label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-teal-300 shadow-sm rounded-lg"
+              />
             </div>
-          </div>
-          <div className="flex-1 mt-12 sm:max-w-lg lg:max-w-md">
-            <form onSubmit={submitForm} className="space-y-5 bg-white p-6 rounded-lg shadow-lg">
-              <div>
-                <label className="font-medium text-lg text-gray-700">{t("contact.form.name")}</label> {/* Dil çevirisi */}
-                <input
-                  type="text"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-teal-300 shadow-sm rounded-lg"
-                />
-              </div>
-              <div>
-                <label className="font-medium text-lg text-gray-700">{t("contact.form.email")}</label> {/* Dil çevirisi */}
-                <input
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-teal-300 shadow-sm rounded-lg"
-                />
-              </div>
-              <div>
-                <label className="font-medium text-lg text-gray-700">{t("contact.form.company")}</label> {/* Dil çevirisi */}
-                <input
-                  type="text"
-                  name="company"
-                  value={formData.company}
-                  onChange={handleChange}
-                  required
-                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-teal-300 shadow-sm rounded-lg"
-                />
-              </div>
-              <div>
-                <label className="font-medium text-lg text-gray-700">{t("contact.form.message")}</label> {/* Dil çevirisi */}
-                <textarea
-                  name="message"
-                  value={formData.message}
-                  onChange={handleChange}
-                  required
-                  className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-teal-300 shadow-sm rounded-lg"
-                ></textarea>
-              </div>
-              <button
-                type="submit"
-                className="w-full px-4 py-2 text-white font-medium text-lg bg-teal-500 hover:bg-teal-600 active:bg-teal-500 rounded-lg duration-150"
-              >
-                {t("contact.form.submit")} {/* Dil çevirisi */}
-              </button>
-            </form>
-          </div>
+            <div>
+              <label className="font-medium text-lg text-gray-700">
+                {t("contact.form.email")}
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-teal-300 shadow-sm rounded-lg"
+              />
+            </div>
+            <div>
+              <label className="font-medium text-lg text-gray-700">
+                {t("contact.form.company")}
+              </label>
+              <input
+                type="text"
+                name="company"
+                value={formData.company}
+                onChange={handleChange}
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-teal-300 shadow-sm rounded-lg"
+              />
+            </div>
+            <div>
+              <label className="font-medium text-lg text-gray-700">
+                {t("contact.form.message")}
+              </label>
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+                className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-teal-300 shadow-sm rounded-lg"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white font-medium text-lg bg-teal-500 hover:bg-teal-600 active:bg-teal-500 rounded-lg duration-150"
+            >
+              {t("contact.form.submit")}
+            </button>
+          </form>
         </div>
       </div>
     </main>
