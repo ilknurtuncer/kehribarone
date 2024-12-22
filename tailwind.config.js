efa019f77b89db6a2f1bc75b44fc96299a917147
Ilknur <ilknurtuncer67@gmail.com>
2024-12-22 03:32:55 +0300

    twjcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Projedeki tüm dosyalar
   ],
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       sans: ['Roboto', 'Arial', 'sans-serif'], // Varsayılan font
-  //       serif: ['Merriweather', 'serif'], // Özel font 
-  //     },
-  //   },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'], // Varsayılan font
+        serif: ['Merriweather', 'serif'], // Özel font 
+      },
+    },
   },
   plugins: [],
 };
