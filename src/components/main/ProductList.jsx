f30124492ff0eb356b4a436df5b028ba9ceafb93
Ilknur <ilknurtuncer67@gmail.com>
2025-02-06 23:27:14 +0300

    ProductList res

--- src/components/main/ProductList.jsx
+++ src/components/main/ProductList.jsx
@@ -7,11 +7,11 @@ const ProductList = () => {
   const { t } = useTranslation(); // Çeviri hook'u
 
   return (
-    <div className="p-12">
-      <h1 className="text-5xl text-yellow-900 font-bold mb-4 text-center">
+    <div className="p-6 sm:p-8 md:p-12">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl text-yellow-900 font-bold mb-6 text-center">
         {t("productList.title")} {/* Çeviri anahtarı */}
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-24">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-12 md:gap-16">
         {productData.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
